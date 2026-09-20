69e5118e3f456704b37be610bdd7ad06f4688640
dhamechayagnik1-rgb <dhamechayagnik1@gmail.com>
2026-09-20 05:56:58 +0000

    form log

--- React_Web/React/src/adminpanel/admin.jsx
+++ React_Web/React/src/adminpanel/admin.jsx
@@ -4,7 +4,8 @@ import { DisplayContext } from '../context'
 function Admin() {
     const {blogs, setBlogs} = useContext(DisplayContext)
 
-
+    console.log(blogs);
+    
 
     return (
         <>
